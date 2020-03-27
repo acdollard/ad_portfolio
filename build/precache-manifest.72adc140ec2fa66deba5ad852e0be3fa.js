ee9d70aa8b374d3033a745d7d6f9b9f54d1390ca
Alex Dollard <alexdollard141@gmail.com>
2020-03-27 10:17:33 -0500

    added build folder to git (was in gitignore)

---
+++ build/precache-manifest.72adc140ec2fa66deba5ad852e0be3fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958aa982db34949240d11b8abf7ea485",
+    "url": "/ad_portfolio/index.html"
+  },
+  {
+    "revision": "42cfbe006c71461155b3",
+    "url": "/ad_portfolio/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "e2a1ed6407ef68561180",
+    "url": "/ad_portfolio/static/js/2.3c3535c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ad_portfolio/static/js/2.3c3535c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42cfbe006c71461155b3",
+    "url": "/ad_portfolio/static/js/main.34428859.chunk.js"
+  },
+  {
+    "revision": "eafa91d7c0d53cbb48e5",
+    "url": "/ad_portfolio/static/js/runtime-main.b143ce2c.js"
+  }
+]);
