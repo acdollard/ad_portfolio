733f9fae176d4d1bf3c68bc3d1e0fc36c1c56faf
Alex Dollard <alexdollard141@gmail.com>
2020-05-03 11:12:44 -0500

    removed skills gauge

--- src/components/About.js
+++ src/components/About.js
@@ -25,29 +25,7 @@ function About() {
                   </div>
                 </div>
               </div>
-              <div className="skill-mf">
-                <p className="title-s">Skill</p>
-                <span>HTML</span> <span className="pull-right">90%</span>
-                <div className="progress">
-                  <div className="progress-bar" role="progressbar" style={{width: "90%" , ariaValuenow:"90" , ariaValuemin:"0" ,
-                    ariaValuemax:"100"}}></div>
-                </div>
-                <span>CSS3</span> <span className="pull-right">75%</span>
-                <div className="progress">
-                  <div className="progress-bar" role="progressbar" style={{width: "75%" , ariaValuenow:"75" , ariaValuemin:"0" ,
-                    ariaValuemax:"100"}}></div>
-                </div>
-                <span>React</span> <span className="pull-right">52%</span>
-                <div className="progress">
-                  <div className="progress-bar" role="progressbar" style={{width: "52%" , ariaValuenow:"52" , ariaValuemin:"0" ,
-                    ariaValuemax:"100"}}></div>
-                </div>
-                <span>JAVASCRIPT</span> <span className="pull-right">65%</span>
-                <div className="progress">
-                  <div className="progress-bar" role="progressbar" style={{width: "65%" , ariaValuenow:"65" , ariaValuemin:"0" ,
-                    ariaValuemax:"100"}}></div>
-                </div>
-              </div>
+
             </div>
             <div className="col-md-6">
               <div className="about-me pt-4 pt-md-0">
