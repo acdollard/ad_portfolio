1755f57a08e6cdcca18a8549b2eb103e9ba02ac3
Alex Dollard <alexdollard141@gmail.com>
2020-05-08 13:55:46 -0500

    added links to github in project descriptions + added Covid-19 tracker

--- src/components/Work.js
+++ src/components/Work.js
@@ -21,7 +21,33 @@ function Work() {
         </div>
       </div>
     </div>
+    
     <div className="row">
+    <div className="col-md-4">
+        <div className="work-box">
+          <a href="img/covid-19-screenshot.png" data-lightbox="gallery-mf">
+            <div className="work-img">
+              <img src="img/covid-19-screenshot.png" alt="" className="img-fluid"/>
+            </div>
+            <div className="work-content">
+              <div className="row">
+                <div className="col-sm-8">
+                  <h2 className="w-title">Covid-19 Tracker</h2>
+                  <div className="w-more">
+                    <span className="w-ctegory">For Tracking Covid-19 Cases:</span><br />
+                    <span className="w-ctegory"><a href="https://github.com/acdollard/covid-19-tracker">Link to github</a></span>
+                  </div>
+                </div>
+                <div className="col-sm-4">
+                  <div className="w-like">
+                    <span className="ion-ios-plus-outline"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
       <div className="col-md-4">
         <div className="work-box">
           <a href="img/star-dogs.png" data-lightbox="gallery-mf">
@@ -33,7 +59,8 @@ function Work() {
                 <div className="col-sm-8">
                   <h2 className="w-title">Star-Dogs</h2>
                   <div className="w-more">
-                    <span className="w-ctegory">A Horoscope App for Dogs:</span>
+                    <span className="w-ctegory">A Horoscope App for Dogs:</span><br />
+                    <span className="w-ctegory"><a href="https://github.com/acdollard/star-dogs">Link to github</a></span>
                   </div>
                 </div>
                 <div className="col-sm-4">
@@ -46,30 +73,7 @@ function Work() {
           </a>
         </div>
       </div>
-      <div className="col-md-4">
-        <div className="work-box">
-          <a href="img/animousity.png" data-lightbox="gallery-mf">
-            <div className="work-img">
-              <img src="img/animousity.png" alt="" className="img-fluid"/>
-            </div>
-            <div className="work-content">
-              <div className="row">
-                <div className="col-sm-8">
-                  <h2 className="w-title">Animousity</h2>
-                  <div className="w-more">
-                    <span className="w-ctegory">For Recommending Anime:</span>
-                  </div>
-                </div>
-                <div className="col-sm-4">
-                  <div className="w-like">
-                    <span className="ion-ios-plus-outline"></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
+
       <div className="col-md-4">
         <div className="work-box">
           <a href="img/employee-directory.png" data-lightbox="gallery-mf">
@@ -81,7 +85,8 @@ function Work() {
                 <div className="col-sm-8">
                   <h2 className="w-title">Employee Directory</h2>
                   <div className="w-more">
-                    <span className="w-ctegory">For Age-Sorting Employers:</span> 
+                    <span className="w-ctegory">For Age-Sorting Employers:</span> <br />
+                    <span className="w-ctegory"><a href="https://github.com/acdollard/employee-directory">Link to github</a></span>
                   </div>
                 </div>
                 <div className="col-sm-4">
